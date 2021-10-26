8aa06a0028e5d2c5967f6695d2c9ec01b8abe74b
Jaeyun Kim <ktwin01@icloud.com>
2021-10-27 01:01:00 +0900

    HW3 problem1 대충 만듬...

--- HW3/problem1/src/Movie.java
+++ HW3/problem1/src/Movie.java
@@ -1,10 +1,29 @@
-
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Movie {
     private String title;
-    public Movie(String title) { this.title = title;}
+    private Set<String> tags;
+    private Map<String, Integer> rates = new HashMap<>();
+    public Movie(String title, Set<String> tags) { this.title = title; this.tags = tags;}
+    public boolean containsTag(String[] tags){
+        Set<String> tagSet = new HashSet<>();
+        for(String tag : tags) tagSet.add(tag);
+        return containsTag(tagSet);
+    }
+    public boolean containsTag(Set<String> tags){
+        return this.tags.containsAll(tags);
+    }
+    public void updateRating(String username, Integer rate){
+        rates.put(username, rate);
+    }
+    public int getDoubledMedieanRate(){
+        List<Integer> list = new ArrayList<>(rates.values());
+        int n = list.size();
+        if(n == 0) return 0;
+
+        Collections.sort(list);  
+        return (list.get((n-1)/2)+list.get(n/2));
+    }
     @Override
     public String toString() {
         return title;
--- HW3/problem1/src/MovieApp.java
+++ HW3/problem1/src/MovieApp.java
@@ -2,49 +2,136 @@
 import java.util.*;
 
 public class MovieApp {
+    private Map<String, Movie> movies = new HashMap<String, Movie>();
+    private Map<String, User> users = new HashMap<String, User>();
+    private Map<String, Map<String, Integer>> ratings = new HashMap<>();
+    private Map<String, List<List<Movie>>> searchHistories = new HashMap<>();
 
     public boolean addMovie(String title, String[] tags) {
-        // TODO sub-problem 1
-        return false;
+        // check title is available
+        if(title == null || movies.containsKey(title)) return false;
+
+        // check tags is not empty
+        if(tags == null || tags.length == 0) return false;
+
+        // make new Movie and store it
+        Set<String> tagSet = new HashSet<>();
+        for(String tag : tags) tagSet.add(tag);
+        Movie movie = new Movie(title, tagSet);
+        
+        movies.put(title, movie);
+        return true;
     }
 
     public boolean addUser(String name) {
-        // TODO sub-problem 1
-        return false;
+        // check name is available
+        if(name == null || users.containsKey(name)) return false;
+
+        User user = new User(name);
+        users.put(name, user);
+        return true;
     }
 
     public Movie findMovie(String title) {
-        // TODO sub-problem 1
-        return new Movie("Forrest Gump");
+        return movies.get(title); // if no key with title return null....
     }
 
     public User findUser(String username) {
-        // TODO sub-problem 1
-        return new User("Tom Hanks");
+        return users.get(username); // if no key with username return null....
     }
 
     public List<Movie> findMoviesWithTags(String[] tags) {
-        // TODO sub-problem 2
-        return new LinkedList<>();
+        // check
+        List<Movie> ret = new LinkedList<Movie>();
+        if(tags == null || tags.length == 0) return ret;
+
+        //search
+        for(String key : movies.keySet()){
+            Movie movie = movies.get(key);
+            if(movie.containsTag(tags)) ret.add(movie);
+        }
+        return ret;
     }
 
     public boolean rateMovie(User user, String title, int rating) {
-        // TODO sub-problem 3
-            return false;
+        // check 
+        if(title == null || user == null) return false;
+        String username = user.toString();
+        if(findUser(username) == null || findMovie(title) == null) return false;
+        if(rating < 0 || rating > 5) return false;
+
+        // rate
+        if(!ratings.containsKey(username)) ratings.put(username, new HashMap<String, Integer>());
+        Map<String, Integer> rateList = ratings.get(username);
+        rateList.put(title, rating);
+        findMovie(title).updateRating(username, rating);
+
+        return true;
     }
 
     public int getUserRating(User user, String title) {
-        // TODO sub-problem 3
-        return -1;
+        // check
+        if(title == null || user == null) return -1;
+        String username = user.toString();
+        if(findUser(username) == null || findMovie(title) == null) return -1;
+
+        // search
+        if(!ratings.containsKey(username)) return 0;
+        Map<String, Integer> rateList = ratings.get(username);
+        if(!rateList.containsKey(title)) return 0;
+        return rateList.get(title);
     }
 
     public List<Movie> findUserMoviesWithTags(User user, String[] tags) {
         // TODO sub-problem 4
-        return new LinkedList<>();
+        List<Movie> ret = new LinkedList<>();
+        if(user == null || tags == null) return ret;
+        String username = user.toString();
+        if(findUser(username) == null) return ret;
+
+        ret = findMoviesWithTags(tags);
+        if(!searchHistories.containsKey(username)) searchHistories.put(username, new LinkedList<List<Movie>>());
+        List<List<Movie>> history = searchHistories.get(username);
+        history.add(ret);
+        return ret;
     }
 
     public List<Movie> recommend(User user) {
         // TODO sub-problem 4
-        return new LinkedList<>();
+        List<Movie> ret = new LinkedList<>();
+        if(user == null) return ret;
+        String username = user.toString();
+        if(findUser(username) == null) return ret;
+
+        List<List<Movie>> history = searchHistories.get(username);
+        if(history == null) return ret;
+        
+        Map<String, Integer> movieCount = new HashMap<>();
+        for(List<Movie> movies : history){
+            if(movies == null) continue;
+            for(Movie movie : movies){
+                Integer count = movieCount.get(movie.toString());
+                if(count == null) count = 0;
+                movieCount.put(movie.toString(), count+1);
+            }
+        }
+        
+        for(String title : movieCount.keySet()){
+            Movie movie = findMovie(title);
+            for(int i=0; i<3; i++){
+                if(ret.size() <= i){
+                    ret.add(movie);
+                    break;
+                }
+                if(movieCount.get(title) < movieCount.get(ret.get(i).toString())) continue;
+                if(movie.getDoubledMedieanRate() < ret.get(i).getDoubledMedieanRate()) continue;
+                if(movie.toString().compareTo(ret.get(i).toString()) > 0) continue;
+                ret.add(i, movie);
+                break;
+            }
+        }
+        if(ret.size() > 3) ret = ret.subList(0, 3);
+
+        return ret;
     }
 }
