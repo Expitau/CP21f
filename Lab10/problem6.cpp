4ae437115d58570bf40747a8f438ca3d1fde7e5c
Jaeyun Kim <ktwin01@icloud.com>
2021-11-10 21:08:17 +0900

    lab 10 fix

--- Lab10/problem6.cpp
+++ Lab10/problem6.cpp
@@ -3,19 +3,18 @@
 using namespace std;
 
 int main(){
-    char str1[60], str2[60], output[110];
+    char str1[110], str2[60];
     cout << "write 1st word:\n";
     cin >> str1;
     cout << "write 2nd word:\n";
     cin >> str2;
 
-    char * str_pointer = str1, * output_pointer = output;
-    while(*str_pointer) *(output_pointer++) = *(str_pointer++);
-    str_pointer = str2;
-    while(*str_pointer) *(output_pointer++) = *(str_pointer++);
-    *output_pointer = 0;
+    char * str1_pointer = str1, * str2_pointer = str2;
+    while(*str1_pointer) str1_pointer++;
+    while(*str2_pointer) *(str1_pointer++) = *(str2_pointer++);
+    *str1_pointer = 0;
 
-    cout << output;
+    cout << str1;
 
     return 0;
 }
