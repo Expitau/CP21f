59f00bfbbe4d8552ca9d8d4d1e6e37181a760d27
Jaeyun Kim <ktwin01@icloud.com>
2021-12-01 23:11:29 +0900

    fix some errors on HW5/1-4

--- HW5/problem1/1-4.cpp
+++ HW5/problem1/1-4.cpp
@@ -1,9 +1,9 @@
 
 int* pascal_triangle(int N) {
-    int * ret = new int[N];
+    int * ret = new int[N]();
     ret[0] = 1;
+
     for(int i=0; i<N; i++) for(int j=i; j>0; j--) ret[j] += ret[j-1];
-    
     return ret;
 }
 
